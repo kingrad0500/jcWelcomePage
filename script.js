b84a737d79779f0be68efca8de345828bd400c24
Ricardo Atoche <72310741+kingrad0500@users.noreply.github.com>
2024-05-14 10:12:42 -0400

    Page only ask for code and not a read terms. Starting with documents logic

--- script.js
+++ script.js
@@ -2,43 +2,21 @@ const pinNumber = "33654";
 
 document.addEventListener("DOMContentLoaded", function () {
     const overlay = document.getElementById("overlay");
-    //const agreeCheckBox = document.getElementById("agreeCheckbox");
     const continueButton = document.getElementById("continueButton");
-    const policiesText = document.getElementById("policiesText");
     const pinNumberEntered = document.getElementById("pinCode");
 
     //Disable continue button by default
     continueButton.disabled = true;
 
-    /*//Listen for the checkbox change
-    agreeCheckBox.addEventListener("change", function () {
-        continueButton.disabled = !this.checked; //enable continue button if checkboxis checked
-    });*/
-
-
-
-
     //show popup on page load
     overlay.style.display = "flex";
 
-    //add scroll event listener to policies text
-    policiesText.addEventListener("scroll", function () {
-        //check if user scrolled to the bottom
-        if (this.scrollHeight - this.scrollTop === this.clientHeight) {
-            if (validateCode(pinNumberEntered.value)) {
-                continueButton.disabled = false; //Enable continue button if scrolled to the bottom
-            }
 
-        }
-        else {
-            continueButton.disabled = true; //otherwise, disable contiunues
-        }
-    });
 
     //Listen for input event on code inpuit field
     pinNumberEntered.addEventListener("input", function () {
-        //Disable continue button when code input changes
-        continueButton.disabled = true;
+        //Enable continue button if the entered code is correct
+        continueButton.disabled = !validateCode(pinNumberEntered.value);
     });
 
     //add event listener to continue button
